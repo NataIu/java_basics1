3432f75529d50d38daa315066913ddc2a6a82174
Nata <teat@test.test>
2020-11-18 00:37:05 +0300

    static methods and variables (regular and additional tasks)

--- 03_ObjectsAndClasses/Cat/src/Cat.java
+++ 03_ObjectsAndClasses/Cat/src/Cat.java
@@ -8,30 +8,48 @@ public class Cat
     private double minWeight;
     private double maxWeight;
 
+    private static int count;
+
     public Cat()
     {
         weight = 1500.0 + 3000.0 * Math.random();
         originWeight = weight;
         minWeight = 1000.0;
         maxWeight = 9000.0;
+        count++;
 
     }
 
     public void meow()
     {
+        if (!isAlive()) {
+            System.out.println("Cat isn't alive, it can't meow!");
+            return;
+        }
         weight = weight - 1;
         System.out.println("Meow");
+        decreaseCountIfNeeded();
     }
 
     public void feed(Double amount)
     {
+        if (!isAlive()) {
+            System.out.println("Cat isn't alive, it can't eat!");
+            return;
+        }
         weight = weight + amount;
         eatenFoodWeight = eatenFoodWeight +amount;
+        decreaseCountIfNeeded();
     }
 
     public void drink(Double amount)
     {
+        if (!isAlive()) {
+            System.out.println("Cat isn't alive, it can't drink!");
+            return;
+        }
         weight = weight + amount;
+        decreaseCountIfNeeded();
     }
 
     public Double getWeight()
@@ -60,7 +78,32 @@ public class Cat
     }
 
     public void pee() {
+        if (!isAlive()) {
+            System.out.println("Cat isn't alive, it can't pee!");
+            return;
+        }
         weight = weight - 1;
         System.out.println("Ready!");
+        decreaseCountIfNeeded();
     }
+
+    private void decreaseCountIfNeeded() {
+        if (!isAlive()) {
+            //думала, надо ли ставить проверку на то,что до последнего действия кошка была живая.
+            //(иначе можем дважды уменьшить count)
+            // Но не стала, т.к. во всех методах, которые меняют вес (и, соответственно, статус)
+            // стоит запрет работы с неживой кошкой
+            count--;
+        }
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    private boolean isAlive() {
+        return getStatus().equals("Sleeping") || getStatus().equals("Playing");
+    }
+
+
 }
--- 03_ObjectsAndClasses/Cat/src/Loader.java
+++ 03_ObjectsAndClasses/Cat/src/Loader.java
@@ -4,22 +4,56 @@ public class Loader
     public static void main(String[] args)
     {
 
+//        Cat cat = new Cat();
+//
+//        System.out.println(cat.getStatus());
+
+        //creating cats
+        System.out.println("Count of cats = "+Cat.getCount());
         Cat vasya = new Cat();
-        System.out.println("Vasya weigt = "+vasya.getWeight()+", eaten food weigt = "+vasya.getEatenFoodWeight());
+        Cat murka = new Cat();
 
-        vasya.feed(100.0);
-        System.out.println("Vasya weigt = "+vasya.getWeight()+", eaten food weigt = "+vasya.getEatenFoodWeight());
+        System.out.println("Count of cats = "+Cat.getCount());
+        Cat petr = new Cat();
+        Cat dymok = new Cat();
+        Cat bonya = new Cat();
 
-        vasya.feed(150.0);
-        System.out.println("Vasya weigt = "+vasya.getWeight()+", eaten food weigt = "+vasya.getEatenFoodWeight());
+        System.out.println("Count of cats = "+Cat.getCount());
 
-        vasya.pee();
-        System.out.println("Vasya weigt = "+vasya.getWeight()+", eaten food weigt = "+vasya.getEatenFoodWeight());
-
-        for (int i = 0; i < 7; i++) {
-            vasya.pee();
+        //feeding too much
+        System.out.println();
+        System.out.println("Before exploding: Vasya status = "+vasya.getStatus()+", Vasya weight = "+ vasya.getWeight());
+        while (! vasya.getStatus().equals("Exploded")) {
+            vasya.feed(100.0);
         }
-        System.out.println("Vasya weigt = "+vasya.getWeight()+", eaten food weigt = "+vasya.getEatenFoodWeight());
+        System.out.println("After exploding: Vasya status = "+vasya.getStatus()+", Vasya weight ="+ vasya.getWeight());
+
+        System.out.println("Count of cats = "+Cat.getCount());
+
+        //testing feeding, drinking, peeing, meowing for exploded cat
+        vasya.feed(10.0);
+        vasya.drink(1.0);
+        vasya.pee();
+        vasya.meow();
+        System.out.println("Test for exploded cat: Vasya status = "+vasya.getStatus()+", Vasya weight ="+ vasya.getWeight());
+
+        //meowing too much
+        System.out.println();
+        System.out.println("Before meowing: Murka status = "+murka.getStatus()+", Murka weight = "+ murka.getWeight());
+        while (! murka.getStatus().equals("Dead")) {
+            murka.meow();
+        }
+        System.out.println("After meowing: Murka status = "+murka.getStatus()+", Murka weight = "+ murka.getWeight());
+
+        //testing feeding, drinking, peeing, meowing for dead cat
+        murka.feed(10.0);
+        murka.drink(1.0);
+        murka.pee();
+        murka.meow();
+        System.out.println("Test for dead cat: Murka status = "+murka.getStatus()+", Murka weight ="+ murka.getWeight());
+
+        System.out.println("Count of cats = "+Cat.getCount());
+
 
     }
 }
