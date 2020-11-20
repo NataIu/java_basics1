8438217b351db7ddd54ec22ec4915fa630ba3483
Nata <teat@test.test>
2020-11-20 17:30:29 +0300

    Homework 4.2

---
+++ 04_NumbersStringsAndDates/homework_4.2/TypesExperiments/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //byte
+        System.out.println("byte: min = "+Byte.MIN_VALUE+", max = "+Byte.MAX_VALUE);
+
+        //short
+        System.out.println("short: min = "+Short.MIN_VALUE+", max = "+Short.MAX_VALUE);
+
+        //int
+        System.out.println("int: min = "+Integer.MIN_VALUE+", max = "+Integer.MAX_VALUE);
+
+        //long
+        System.out.println("long: min = "+Long.MIN_VALUE+", max = "+Long.MAX_VALUE);
+
+        //float
+        System.out.println("float: min = "+Float.MIN_VALUE+", max = "+Float.MAX_VALUE);
+
+        //double
+        System.out.println("double: min = "+Double.MIN_VALUE+", max = "+Double.MAX_VALUE);
+
+
+    }
+}
