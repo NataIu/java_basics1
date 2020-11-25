e84abd2a0e0b4adcdd244fe061b80bda91a74e9b
Nata <teat@test.test>
2020-11-25 22:15:13 +0300

    Homework 4.4 - task 3

--- 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/FullNameFormatter/src/main/java/Main.java
@@ -2,17 +2,64 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    private static final String VALID_SYMBOLS = "АБВГДЕЁЖЗИЙУЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя-";
+    private static final String FORMAT_ERROR_MESSAGE = "Введенная строка не является ФИО";
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
-      //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+    public static void main(String[] args) {
+
+        String surname;
+        String name;
+        String middleName;
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+
+            input = input.trim();
+
+            //проверим, что есть хотя бы два разных пробела (т.е. >=3 слов):
+            if (input.indexOf(' ') == 0 || input.indexOf(' ') == input.lastIndexOf(' ')) {
+                System.out.println(FORMAT_ERROR_MESSAGE);
+                continue;
+            }
+
+            surname = input.substring(0, input.indexOf(' '));
+            name = input.substring(input.indexOf(' ') + 1, input.lastIndexOf(' '));
+            middleName = input.substring(input.lastIndexOf(' ') + 1);
+
+            if (name.indexOf(' ') > 0) {
+                //было больше 3 слов
+                System.out.println(FORMAT_ERROR_MESSAGE);
+                continue;
+            }
+
+            if (!isValidWord(surname) || !isValidWord(name) || !isValidWord(middleName)) {
+                System.out.println(FORMAT_ERROR_MESSAGE);
+                continue;
+            }
+
+            System.out.println("Фамилия: " + surname);
+            System.out.println("Имя: " + name);
+            System.out.println("Отчество: " + middleName);
+        }
+
     }
-  }
+
+
+    private static boolean isValidWord(String text) {
+
+        boolean isValidSymbol = true;
+        int i = 0;
+        while (i < text.length() && isValidSymbol) {
+            if (VALID_SYMBOLS.indexOf(text.charAt(i)) < 0) {
+                isValidSymbol = false;
+            }
+            i++;
+        }
+        return isValidSymbol;
+    }
 
 }
