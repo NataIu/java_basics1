c9c13584c37bcfd2c2e2e6a973fcb302788f7d85
Nata <teat@test.test>
2020-11-25 21:40:35 +0300

    Homework 4.4 - task 2

--- 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.4/TotalEarningsCalculator/src/main/java/Main.java
@@ -1,9 +1,40 @@
 public class Main {
 
+  private static final String ALL_NUMBERS = "0123456789";
+
   public static void main(String[] args) {
 
-    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
-  }
+    String currentWord;
+    boolean isNumber;
+    int i;
+    int summ = 0;
 
+    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+    text = text.trim();
+
+    while (text.length() > 0) {
+
+      //отрежем первое слово от строки
+      if (text.indexOf(' ') >= 0) {
+        currentWord = text.substring(0, text.indexOf(' '));
+        text = text.substring(text.indexOf(' ')).trim();
+      } else {
+        currentWord = text;
+        text = "";
+      }
+
+      //проверим, является ли слово числом. Если является - добавим к общей сумме
+      isNumber = true;
+      i = 0;
+      while (i < currentWord.length() && isNumber) {
+        if (ALL_NUMBERS.indexOf(currentWord.charAt(i)) < 0) {
+          isNumber = false;
+        }
+        i++;
+      }
+      summ= summ+ (isNumber ? Integer.parseInt(currentWord) :0 );
+    }
+    System.out.println(""+summ);
+
+  }
 }
