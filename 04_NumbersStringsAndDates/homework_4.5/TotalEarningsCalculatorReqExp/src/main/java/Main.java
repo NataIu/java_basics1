ecebfc276f0a4ff128816bd8a7c489f941a2bc42
Nata <teat@test.test>
2020-11-26 21:50:58 +0300

    Homework 4.5 - task 1

--- 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/TotalEarningsCalculatorReqExp/src/main/java/Main.java
@@ -1,12 +1,26 @@
 public class Main {
 
   public static void main(String[] args) {
-
+//    calculateSalarySum("  Маша - 100, Оля - 250, Коля 8  ");
+        calculateSalarySum("  Маша  ");
   }
 
   public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
+    //TODO: реализуйте
+
+    int result = 0;
+
+    text = text.replaceAll("[^0-9 ]","").trim();
+    if (text.length() > 0) {
+      String[] summs = text.split("\\s+");
+
+      for (int i = 0; i < summs.length; i++) {
+        result += Integer.parseInt(summs[i]);
+      }
+    }
+
+    System.out.println(result);
+    return result;
   }
 
 }
