8426b8b66b0f32413ee5ebbd5ef06310f6e69d5b
Nata <teat@test.test>
2020-11-30 22:42:14 +0300

    Homework 4.5 - task 5

--- 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/MaskingInformationReqExp/src/main/java/Main.java
@@ -1,11 +1,15 @@
 public class Main {
 
     public static void main(String[] args) {
+        searchAndReplaceDiamonds("fdfsfjk<", "*!");
 
     }
 
     public static String searchAndReplaceDiamonds(String text, String placeholder) {
         // TODO: реализовать метод, если в строке нет <> - вернуть строку без изменений
+
+        text = text.replaceAll("<[^<>]*>", placeholder);
+        System.out.println(text);
         return text;
     }
 
