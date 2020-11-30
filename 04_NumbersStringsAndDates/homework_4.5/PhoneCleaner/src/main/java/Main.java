0ed52758efbe23ff8ed2ac8b2fa37882eae63124
Nata <teat@test.test>
2020-11-30 22:17:53 +0300

    Homework 4.5 - task 4

--- 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
+++ 04_NumbersStringsAndDates/homework_4.5/PhoneCleaner/src/main/java/Main.java
@@ -2,16 +2,25 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    Scanner scanner = new Scanner(System.in);
-    while (true) {
-      String input = scanner.nextLine();
-      if (input.equals("0")) {
-        break;
-      }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("0")) {
+                break;
+            }
+
+            input = input.replaceAll("[ //(//)-//+]", "").trim();
+            if (input.matches("[78]?[0-9]{10}")) {
+                if (input.length() == 11) {
+                  input = input.substring(1);
+                }
+                System.out.println("7" + input);
+            } else {
+                System.out.println("Неверный формат номера");
+            }
+        }
     }
-  }
 
 }
