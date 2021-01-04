aa035e6e6433fd6a4740b90cdb2d417db215935a
Nata <teat@test.test>
2021-01-04 22:01:27 +0300

    Homework 5.1 - task 1

--- 05_ArraysAndCollections/homework_5.1/ReverseArray/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.1/ReverseArray/src/main/java/Main.java
@@ -2,5 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         String line = "Каждый охотник желает знать, где сидит фазан";
+        String[] colorWords = line.split("\\p{Punct}?\\s");
+        ReverseArray.reverse(colorWords);
+        for (String colorWord:colorWords) {
+            System.out.println(colorWord);
+        }
     }
 }
--- 05_ArraysAndCollections/homework_5.1/ReverseArray/src/main/java/ReverseArray.java
+++ 05_ArraysAndCollections/homework_5.1/ReverseArray/src/main/java/ReverseArray.java
@@ -2,6 +2,14 @@ public class ReverseArray {
 
     //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
     public static String[] reverse (String[] strings){
+
+        String tempElement = new String();
+
+        for (int i = 0; i < (strings.length/2); i++) { //тут просто делим, т.к. остаток отбрасывается
+            tempElement = strings[i];
+            strings[i] = strings[strings.length-i-1];
+            strings[strings.length-i-1] = tempElement;
+        }
         return strings;
     }
 }
