5767a1d8ca65f1c7c0ae1e9e70e5d4499ad0ef37
Nata <teat@test.test>
2021-01-06 22:41:37 +0300

    Homework 5.1 - additional task

--- 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/Main.java
+++ 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/Main.java
@@ -1,14 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        char[][] twoDimensionalArray = TwoDimensionalArray.getTwoDimensionalArray(5);
-        for (char[] line:twoDimensionalArray) {
+//        char[][] twoDimensionalArray = TwoDimensionalArray.getTwoDimensionalArray(5);
+//        for (char[] line:twoDimensionalArray) {
+//            for (int i = 0; i < line.length; i++) {
+//                System.out.print(line[i]);
+//            }
+//            System.out.println();
+//
+//        }
+//        //Распечатайте сгенерированный в классе TwoDimensionalArray.java двумерный массив
+
+        double[][] twoDimensionalArray = TwoDimensionalArray.getTwoDimensionalArrayWithBumbers(10);
+        for (double[] line:twoDimensionalArray) {
             for (int i = 0; i < line.length; i++) {
-                System.out.print(line[i]);
+                System.out.print(line[i]+" ");
             }
             System.out.println();
-
         }
-        //Распечатайте сгенерированный в классе TwoDimensionalArray.java двумерный массив
+        System.out.println();
+        TwoDimensionalArray.printValuesDiagonally(twoDimensionalArray);
+
     }
 }
--- 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
+++ 05_ArraysAndCollections/homework_5.1/TwoDimensionalArray/src/main/java/TwoDimensionalArray.java
@@ -1,8 +1,8 @@
 public class TwoDimensionalArray {
+
     public static char symbol = 'X';
 
     public static char[][] getTwoDimensionalArray(int size) {
-
 
         char[][] resultArray = new char[size][size];
         for (int i = 0; i < size; i++) {
@@ -13,6 +13,7 @@ public class TwoDimensionalArray {
     }
 
     private static void fillOneLine(int lineNumber, int size, char[][] resultArray) {
+
         for (int i = 0; i < size; i++) {
             if ((lineNumber==i) || (lineNumber==size-i-1)) {
                 resultArray[lineNumber][i] = 'X';
@@ -22,4 +23,52 @@ public class TwoDimensionalArray {
             }
         }
     }
+
+    public static double[][] getTwoDimensionalArrayWithBumbers(int size) {
+
+        double[][] resultArray = new double[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                resultArray[i][j] = ((double) j)/10 + i;
+            }
+        }
+
+        return resultArray;
+    }
+
+    public static void printValuesDiagonally(double[][] array) {
+
+        //считаю, что массив n*n, т.к. array.length равно и длине, и высоте
+        for (int i = 0; i < 2 * array.length - 1; i++) {
+            printLine(i,array);
+            System.out.println();
+        }
+
+    }
+
+    private static void printLine(int lineIndex, double[][] array) {
+
+        if (lineIndex < array.length) {
+            for (int i = 0; i <= lineIndex; i++) {
+                if (lineIndex % 2 == 1) {
+                    System.out.print(array[i][lineIndex - i] + " ");
+                } else {
+                    System.out.print(array[lineIndex - i][i] + " ");
+                }
+            }
+        }
+        else {
+
+            for (int i = (lineIndex - array.length + 1); i < array.length; i++) {
+                if (lineIndex % 2 == 1) {
+                    System.out.print(array[i][lineIndex - i] + " ");
+                } else {
+                    System.out.print(array[lineIndex - i] [i]+ " ");
+                }
+            }
+
+        }
+
+    }
+
 }
