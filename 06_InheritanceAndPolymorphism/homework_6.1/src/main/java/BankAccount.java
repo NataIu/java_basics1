6fb8666ca8f8619d9ca796d787d5c484ffe40d83
Nata <teat@test.test>
2021-01-16 14:26:19 +0300

    Homework 6.1 - task2

--- 06_InheritanceAndPolymorphism/homework_6.1/src/main/java/BankAccount.java
+++ 06_InheritanceAndPolymorphism/homework_6.1/src/main/java/BankAccount.java
@@ -13,9 +13,35 @@ public class BankAccount {
     }
 
     public void take(double amountToTake) {
-        if ( Double.compare(amount, amountToTake) > 0) {
+        if ( Double.compare(amount, amountToTake) >= 0) {
             amount = amount - amountToTake;
         }
+    }
+
+    public boolean send(BankAccount receiver, double amount) {
+
+        boolean result = false;
+        double firstAmountBeforeOperations = getAmount();
+        double secondAmountBeforeOperations = 0;
+
+        //снимем деньги с первого счета
+        take(amount);
+        if (Double.compare(firstAmountBeforeOperations,getAmount()) == 0) {
+            //не получилось снять деньги с первого счета
+            return result;
+        }
+
+        //положим деньги на второй счет
+        secondAmountBeforeOperations = receiver.getAmount();
+        receiver.put(amount);
+        if (Double.compare(secondAmountBeforeOperations,receiver.getAmount()) == 0) {
+            //не получилось положить деньги на второй счет
+            amount =  firstAmountBeforeOperations;
+            return result;
+        }
+
+        result = true;
+        return result;
     }
 
 }
--- 06_InheritanceAndPolymorphism/homework_6.1/src/main/java/Main.java
+++ 06_InheritanceAndPolymorphism/homework_6.1/src/main/java/Main.java
@@ -1,22 +1,26 @@
 public class Main {
 
     public static void main(String[] args) {
-//        BankAccount bankAccount = new BankAccount();
+        CardAccount bankAccount = new CardAccount();
+        CardAccount secondAccount = new CardAccount();
 //        CardAccount bankAccount = new CardAccount();
-        DepositAccount bankAccount = new DepositAccount();
+//        DepositAccount bankAccount = new DepositAccount();
 
         bankAccount.put(100000);
-        System.out.println(bankAccount.getAmount());
+        System.out.println("first: "+bankAccount.getAmount()+", second: "+secondAccount.getAmount());
 
-        bankAccount.put(10000);
-        System.out.println(bankAccount.getAmount());
+        bankAccount.send(secondAccount,100000);
+        System.out.println("first: "+bankAccount.getAmount()+", second: "+secondAccount.getAmount());
 
-        bankAccount.put(-1);
-        System.out.println(bankAccount.getAmount());
-
-
-        bankAccount.take(100);
-        System.out.println(bankAccount.getAmount());
+//        bankAccount.put(10000);
+//        System.out.println(bankAccount.getAmount());
+//
+//        bankAccount.put(-1);
+//        System.out.println(bankAccount.getAmount());
+//
+//
+//        bankAccount.take(100);
+//        System.out.println(bankAccount.getAmount());
 
     }
 }
