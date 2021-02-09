b9ba6c8c17763fc2fa1f46f73506c59d368805c4
Nata <teat@test.test>
2021-02-09 23:02:18 +0300

    Homework 9.1 - task 1

--- 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/FileUtils.java
@@ -1,6 +1,39 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FileUtils {
 
-    public static long calculateFolderSize(String path) {
-        return 0;
+    public static long calculateFolderSize(String path)  {
+
+        long result = 0;
+
+        //получить размер всех файлов в указанной папке и ее подпапках,
+        // реализация этого должна быть написана в методе calculateFolderSize() класса FileUtils;
+
+        //программа должна перехватывать все исключения, возникающие при ошибках чтения файлов и папок,
+        // и выводить сообщение об ошибке с трассировкой стека (stack trace).
+
+
+        Path dirPath = Path.of(path);
+        if (Files.isDirectory(dirPath)) {
+            try {
+                result = Files.walk(dirPath)
+                        .filter(Files::isRegularFile)
+                        .map(path1 -> {
+                            try {
+                                return Files.size(path1);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            return null;
+                        })
+                        .reduce(Long::sum)
+                        .orElseGet(null);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
     }
 }
--- 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.1/src/main/java/Main.java
@@ -1,5 +1,32 @@
-public class Main {
-    public static void main(String[] args) {
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+
+//        получать через консоль путь от пользователя до папки;
+        Scanner scanner = new Scanner(System.in);
+        String fileName = scanner.nextLine();
+
+        long commonSize = FileUtils.calculateFolderSize(fileName);
+
+        //выводить полученную сумму файлов в удобочитаемом виде — в байтах, килобайтах, мегабайтах или гигабайтах;
+        System.out.println("Размер папки "+fileName+" cоставляет "+sizeToText(commonSize));
+
+    }
+
+    private static String sizeToText(long commonSize) {
+        if (commonSize < 1024 ) {
+            return ""+commonSize+" б";
+        }
+        else  if (commonSize < 1024*1024 ) {
+            return ""+commonSize/1024+" Кб";
+        }
+        else  if (commonSize < 1024*1024*1024 ) {
+            return ""+commonSize/1024/1024+" Мб";
+        }
+        else {
+            return ""+commonSize/1024/1024/1024+" Гб";
+        }
     }
 }
