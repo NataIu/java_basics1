161b0e76b5393ed34019fa1cff0b2dc0c958a2aa
Nata <teat@test.test>
2021-02-14 12:16:29 +0300

    Homework 9.2 - task 1

--- 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
@@ -1,5 +1,38 @@
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+
 public class FileUtils {
+
+    static Logger logger = LogManager.getLogger(FileUtils.class);
+
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
-        // TODO: write code copy content of sourceDirectory to destinationDirectory
+
+        CharSequence source = (CharSequence) sourceDirectory;
+        CharSequence destination = (CharSequence) destinationDirectory;
+
+        try {
+            Files.walk(Path.of(sourceDirectory)).forEach(s -> {
+                try {
+                    Files.copy(s, changePath(source, destination, s));
+                } catch (IOException e) {
+                    logger.error(e.getMessage());
+                }
+            });
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    private static Path changePath(CharSequence source, CharSequence destination, Path currentFile) {
+
+        String tmp = currentFile.toString();
+        return Path.of(tmp.replace(source, destination));
+
     }
 }
--- 09_FilesAndNetwork/homework_9.2/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.2/src/main/java/Main.java
@@ -1,4 +1,12 @@
+import java.nio.file.Files;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String sourceDirectory = scanner.nextLine();
+        String destinationDirectory = scanner.nextLine();
+        FileUtils.copyFolder(sourceDirectory,destinationDirectory);
     }
 }
