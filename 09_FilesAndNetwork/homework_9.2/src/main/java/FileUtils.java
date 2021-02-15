d9b8fc7c35780ad9b15fc62dc5433831f88723fb
Nata <teat@test.test>
2021-02-15 22:33:00 +0300

    Homework 9.2 - task 1, v2

--- 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
+++ 09_FilesAndNetwork/homework_9.2/src/main/java/FileUtils.java
@@ -3,8 +3,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.nio.file.CopyOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
 
 
 public class FileUtils {
@@ -15,11 +18,16 @@ public class FileUtils {
 
         CharSequence source = (CharSequence) sourceDirectory;
         CharSequence destination = (CharSequence) destinationDirectory;
+        StandardCopyOption copyOption;
+
+        StringBuilder stringBuilder = getListOfExistingFiles(sourceDirectory, source, destination);
+        copyOption = getCopyOption(stringBuilder);
 
         try {
+            StandardCopyOption finalCopyOption = copyOption;
             Files.walk(Path.of(sourceDirectory)).forEach(s -> {
                 try {
-                    Files.copy(s, changePath(source, destination, s));
+                    Files.copy(s, changePath(source, destination, s), finalCopyOption);
                 } catch (IOException e) {
                     logger.error(e.getMessage());
                 }
@@ -27,6 +35,49 @@ public class FileUtils {
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
+    }
+
+    private static StandardCopyOption getCopyOption(StringBuilder stringBuilder) {
+
+        StandardCopyOption copyOption = StandardCopyOption.COPY_ATTRIBUTES;
+        String answer = "";
+
+        if (stringBuilder.length() > 0) {
+            while (!answer.equalsIgnoreCase("y") && !answer.equalsIgnoreCase("n")) {
+                System.out.println("These files are already exist in destionation directory: ");
+                System.out.println(stringBuilder);
+                System.out.println("Replace files (y/n)?  ");
+
+                Scanner scanner = new Scanner(System.in);
+                answer = scanner.nextLine();
+            }
+            if (answer.equalsIgnoreCase("y")) {
+                copyOption = StandardCopyOption.REPLACE_EXISTING;
+            }
+        }
+        return copyOption;
+    }
+
+    private static StringBuilder getListOfExistingFiles(String sourceDirectory, CharSequence source, CharSequence destination) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try {
+            Files.walk(Path.of(sourceDirectory))
+                    .filter(Files::isRegularFile)
+                    .filter(s -> {
+                        try {
+                            Path newPath = changePath(source, destination, s);
+                            return Files.exists(newPath) && ((Long) Files.size(s)).equals(((Long) Files.size(newPath)));
+                        } catch (IOException e) {
+                            logger.error(e.getMessage());
+                        }
+                        return false;
+                    })
+                    .forEach(s->stringBuilder.append(s.toString()+"\n"));
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+        return stringBuilder;
     }
 
     private static Path changePath(CharSequence source, CharSequence destination, Path currentFile) {
