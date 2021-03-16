575f368ac2fe26433767d12d1150c64b4a5981ef
Nata <lushnikova-natalia@rambler.ru>
2021-03-16 20:58:21 +0300

    Homework 9.5 - task 1, v2

--- 09_FilesAndNetwork/homework_9.5/src/main/java/Main.java
+++ 09_FilesAndNetwork/homework_9.5/src/main/java/Main.java
@@ -32,7 +32,7 @@ public class Main {
 
 
         getMetroSchemeFromWebSite(stationIndexFromSite);
-        saveMetroSchemeTJsonFile(stationIndexFromSite, filePath);
+        saveMetroSchemeToJsonFile(stationIndexFromSite, filePath);
 
         readStationsFromJsonFile(stationIndexFromJson, filePath);
 
@@ -46,9 +46,8 @@ public class Main {
         //2. Парсит полученную страницу и получает из неё:
         //Линии московского метро (получаете имя линии, номер линии, цвет парсить не надо).
         //Станции московского метро (получаете имя станции, номер линии).
-
-        String linesDataPath = "div.js-toggle-depend>span";
-        String stationsDataPath = "div.js-metro-stations.t-metrostation-list-table";
+        //Переходы между станциями
+        
         Document doc = null;
         try {
             doc = Jsoup.connect(MOS_METRO_PATH).maxBodySize(0).get();
@@ -56,7 +55,14 @@ public class Main {
             logger.error(e.getMessage());
         }
 
+        readLinesFromSite(stationIndexFromSite, doc);
+        readStationsAndConnectionsFromSite(stationIndexFromSite, doc);
+
+    }
+
+    private static void readLinesFromSite(StationIndex stationIndexFromSite, Document doc) {
         //прочитаем с сайта все линии и создадим их как объекты
+        String linesDataPath = "div.js-toggle-depend>span";
         Elements lines = doc.select(linesDataPath);
         lines.stream()
                 .forEach(s -> {
@@ -65,9 +71,18 @@ public class Main {
                     Line line = new Line(lineNumber, lineName);
                     stationIndexFromSite.addLine(line);
                 });
+    }
 
+    private static void readStationsAndConnectionsFromSite(StationIndex stationIndexFromSite, Document doc) {
+        //прочитаем с сайта все станции и переходы между ними и создадим их как объекты
+        String stationsDataPath = "div.js-metro-stations.t-metrostation-list-table";
         Elements linesWithStations = doc.select(stationsDataPath);
-        //прочитаем с сайта все станции и создадим их как объекты
+
+        createStationsTakenFromSite(stationIndexFromSite, linesWithStations);
+        createConnectionsTakenFromSite(stationIndexFromSite, linesWithStations);
+    }
+
+    private static void createStationsTakenFromSite(StationIndex stationIndexFromSite, Elements linesWithStations) {
         for (Element lineWithStations : linesWithStations) {
             String lineNumber = lineWithStations.attr("data-line");
             Line currentLine = stationIndexFromSite.getLine(lineNumber);
@@ -78,8 +93,9 @@ public class Main {
                 stationIndexFromSite.addStation(station);
             });
         }
+    }
 
-        //прочитаем с сайта все переходы между линиями и  создадим их как объекты
+    private static void createConnectionsTakenFromSite(StationIndex stationIndexFromSite, Elements linesWithStations) {
         for (Element lineWithStations : linesWithStations) {
             String lineNumber = lineWithStations.attr("data-line");
 
@@ -87,75 +103,56 @@ public class Main {
                 int dataSize = s.children().get(0).children().size();
 
                 if (dataSize > 2) {
-
-                    String stationName = s.children().get(0).children().get(1).text();
-                    Station station = stationIndexFromSite.getStation(stationName, lineNumber);
-                    List <Station> connectedStations = new ArrayList<>();
-                    connectedStations.add(station);
-
-                    for (int i = 2; i < dataSize; i++) {
-
-                        //номер линии, на которую переходим
-                        String connectionLineNumber = s.children().get(0).children().get(2).attributes().get("class");
-                        connectionLineNumber = connectionLineNumber.replaceAll("t-icon-metroln ln-", "");
-
-                        //имя станции, на которую переходим
-                        String connectionStationText = s.children().get(0).children().get(2).attributes().get("title");
-                        connectionStationText = connectionStationText.substring(connectionStationText.indexOf('«') + 1);
-                        connectionStationText = connectionStationText.substring(0, connectionStationText.indexOf('»'));
-
-                        Station connectedStation = stationIndexFromSite.getStation(connectionStationText, connectionLineNumber);
-                        connectedStations.add(connectedStation);
-                    }
-
-                    stationIndexFromSite.addConnection(connectedStations);
+                    createConnectionFromSiteForChosenStation(stationIndexFromSite, lineNumber, s, dataSize);
                 }
             });
         }
-
     }
 
-    public static void saveMetroSchemeTJsonFile(StationIndex stationIndexFromSite, String filePath) {
+    private static void createConnectionFromSiteForChosenStation(StationIndex stationIndexFromSite, String lineNumber, Element element, int dataSize) {
+
+        List <Station> connectedStations = new ArrayList<>();
+
+        //добавим основную станцию
+        String stationName = element.children().get(0).children().get(1).text();
+        Station station = stationIndexFromSite.getStation(stationName, lineNumber);
+        connectedStations.add(station);
+
+        //добавим все переходы с нее
+        for (int i = 2; i < dataSize; i++) {
+
+            Station connectedStation = findConnectedStationBySiteInformation(stationIndexFromSite, element);
+            connectedStations.add(connectedStation);
+        }
+
+        stationIndexFromSite.addConnection(connectedStations);
+    }
+
+    private static Station findConnectedStationBySiteInformation(StationIndex stationIndexFromSite, Element element) {
+        //номер линии, на которую переходим
+        String connectionLineNumber = element.children().get(0).children().get(2).attributes().get("class");
+        connectionLineNumber = connectionLineNumber.replaceAll("t-icon-metroln ln-", "");
+
+        //имя станции, на которую переходим
+        String connectionStationText = element.children().get(0).children().get(2).attributes().get("title");
+        connectionStationText = connectionStationText.substring(connectionStationText.indexOf('«') + 1);
+        connectionStationText = connectionStationText.substring(0, connectionStationText.indexOf('»'));
+
+        Station connectedStation = stationIndexFromSite.getStation(connectionStationText, connectionLineNumber);
+        return connectedStation;
+    }
+
+
+    public static void saveMetroSchemeToJsonFile(StationIndex stationIndexFromSite, String filePath) {
         //3. Создаёт и записывает на диск JSON-файл со списком станций по линиям и списком линий по формату JSON-файла из проекта SPBMetro (файл map.json).
         JSONObject jsonObject = new JSONObject();
 
         JSONObject jsonObjectStations = new JSONObject();
-        JSONArray jsonConnectionsArray = new JSONArray();
         JSONArray jsonObjectLinesArray = new JSONArray();
+        JSONArray jsonConnectionsArray = new JSONArray();
 
-        for (Map.Entry<String, Line> lineEntry : stationIndexFromSite.getNumber2line().entrySet()) {
-            JSONArray stationsListArray = new JSONArray();
-            stationIndexFromSite.getStationsOnLine(lineEntry.getValue()).stream()
-                    .forEach(s -> stationsListArray.add(s.getName()));
-            jsonObjectStations.put(lineEntry.getValue().getNumber(), stationsListArray);
-
-            JSONObject lineNumberObject = new JSONObject();
-            lineNumberObject.put("number", lineEntry.getValue().getNumber());
-            lineNumberObject.put("name", lineEntry.getValue().getName());
-
-            jsonObjectLinesArray.add(lineNumberObject);
-
-        }
-
-        stationIndexFromSite.getConnections().entrySet().stream().forEach(s->{
-
-            JSONArray jsonArrayConn = new JSONArray();
-
-            JSONObject connectionObject = new JSONObject();
-            connectionObject.put("line", s.getKey().getLine().getNumber());
-            connectionObject.put("station", s.getKey().getName());
-            jsonArrayConn.add(connectionObject);
-
-            for (Station station : s.getValue()) {
-                connectionObject = new JSONObject();
-                connectionObject.put("line", station.getLine().getNumber());
-                connectionObject.put("station", station.getName());
-                jsonArrayConn.add(connectionObject);
-
-            }
-            jsonConnectionsArray.add(jsonArrayConn);
-
-        });
+        prepareStationsAndLinesForJson(stationIndexFromSite, jsonObjectStations, jsonObjectLinesArray);
+        prepareConnectionsForJson(stationIndexFromSite, jsonConnectionsArray);
 
         jsonObject.put("stations", jsonObjectStations);
         jsonObject.put("connections", jsonConnectionsArray);
@@ -172,20 +169,45 @@ public class Main {
 
     }
 
+    private static void prepareStationsAndLinesForJson(StationIndex stationIndexFromSite, JSONObject jsonObjectStations, JSONArray jsonObjectLinesArray) {
+        for (Map.Entry<String, Line> lineEntry : stationIndexFromSite.getNumber2line().entrySet()) {
+            JSONArray stationsListArray = new JSONArray();
+            stationIndexFromSite.getStationsOnLine(lineEntry.getValue()).stream()
+                    .forEach(s -> stationsListArray.add(s.getName()));
+            jsonObjectStations.put(lineEntry.getValue().getNumber(), stationsListArray);
 
-    public static void printStationsQuantityOnLine(StationIndex stationIndexFromJson) {
-    //4 (2) выводит в консоль количество станций на каждой линии.
-        stationIndexFromJson.getNumber2line().entrySet().stream()
-                .forEach(s -> System.out.println(s.getValue().getName() + ": " + stationIndexFromJson.getStationsOnLine(s.getValue()).size()));
+            JSONObject lineNumberObject = new JSONObject();
+            addLineNumber(lineNumberObject,"number", lineEntry.getValue().getNumber());
+            addLineOrStationName(lineNumberObject,"name", lineEntry.getValue().getName());
+
+            jsonObjectLinesArray.add(lineNumberObject);
+
+        }
     }
 
-    public static void printConnectionsQuantity(StationIndex stationIndexFromJson) {
-  //5 выводит в консоль общее количество переходов
-        //формула: если переход между n станциями, то всего n*(n-1)/2 переходов
-        //т.к. для каждой из n станций делаю подсчет, то  нужно еще делить на n
-        //итого суммирую (n-1)/2
-        double result = stationIndexFromJson.getConnections().values().stream().mapToInt(stations -> stations.size()  / 2).sum();
-        System.out.println("Всего переходов: "+ (int) result);
+    private static void prepareConnectionsForJson(StationIndex stationIndexFromSite, JSONArray jsonConnectionsArray) {
+
+        stationIndexFromSite.getConnections().entrySet().stream().forEach(s -> {
+            JSONArray jsonArrayConn = new JSONArray();
+            addConnectionToJsonConnectionArray(jsonArrayConn, s.getKey());
+            s.getValue().stream().forEach(station-> addConnectionToJsonConnectionArray(jsonArrayConn, station));
+            jsonConnectionsArray.add(jsonArrayConn);
+        });
+    }
+
+    private static void addConnectionToJsonConnectionArray(JSONArray jsonArrayConn, Station station) {
+        JSONObject connectionObject = new JSONObject();
+        addLineNumber(connectionObject,"line", station.getLine().getNumber());
+        addLineOrStationName(connectionObject,"station", station.getName());
+        jsonArrayConn.add(connectionObject);
+    }
+
+    private static void addLineNumber(JSONObject jsonObject, String tag, String number) {
+        jsonObject.put(tag, number);
+    }
+
+    private static void addLineOrStationName(JSONObject jsonObject, String tag, String name) {
+        jsonObject.put(tag, name);
     }
 
 
@@ -268,5 +290,23 @@ public class Main {
         }
         return builder.toString();
     }
+
+
+    public static void printStationsQuantityOnLine(StationIndex stationIndexFromJson) {
+        //4 (2) выводит в консоль количество станций на каждой линии.
+        stationIndexFromJson.getNumber2line().entrySet().stream()
+                .forEach(s -> System.out.println(s.getValue().getName() + ": " + stationIndexFromJson.getStationsOnLine(s.getValue()).size()));
+    }
+
+    public static void printConnectionsQuantity(StationIndex stationIndexFromJson) {
+        //5 выводит в консоль общее количество переходов
+        //формула: если переход между n станциями, то всего n*(n-1)/2 переходов
+        //т.к. для каждой из n станций делаю подсчет, то  нужно еще делить на n
+        //итого суммирую (n-1)/2
+        double result = stationIndexFromJson.getConnections().values().stream().mapToInt(stations -> stations.size()  / 2).sum();
+        System.out.println("Всего переходов: "+ (int) result);
+    }
+
+
 
 }
