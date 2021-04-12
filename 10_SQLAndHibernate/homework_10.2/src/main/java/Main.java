14754783753d3ea5c353c764ad308c66691bc8a4
Nata <lushnikova-natalia@rambler.ru>
2021-04-12 23:04:14 +0300

    Homework 10.2 - task 1, v2

---
+++ 10_SQLAndHibernate/homework_10.2/src/main/java/Main.java
@@ -0,0 +1,30 @@
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml").build();
+        Metadata metadata = new MetadataSources(registry).getMetadataBuilder().build();
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+
+        Session session = sessionFactory.openSession();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите id курса:");
+        int id = scanner.nextInt();
+        s
+        Course course = session.get(Course.class,id);
+        System.out.println(course.getName()+" - "+ course.getStudentsCount());
+
+        sessionFactory.close();
+    }
+}
