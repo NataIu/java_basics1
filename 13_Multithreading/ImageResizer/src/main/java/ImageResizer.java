7fae768c0b44cb557658b15ad59a864df8da5d73
Nata <lushnikova-natalia@rambler.ru>
2021-05-30 11:50:41 +0300

    Homework 13.1 - task 2, v1

--- 13_Multithreading/ImageResizer/src/main/java/ImageResizer.java
+++ 13_Multithreading/ImageResizer/src/main/java/ImageResizer.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import org.imgscalr.Scalr;
+
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -27,23 +29,11 @@ public class ImageResizer extends Thread{
                     continue;
                 }
 
-
                 int newHeight = (int) Math.round(
                         image.getHeight() / (image.getWidth() / (double) newWidth)
                 );
-                BufferedImage newImage = new BufferedImage(
-                        newWidth, newHeight, BufferedImage.TYPE_INT_RGB
-                );
 
-                int widthStep = image.getWidth() / newWidth;
-                int heightStep = image.getHeight() / newHeight;
-
-                for (int x = 0; x < newWidth; x++) {
-                    for (int y = 0; y < newHeight; y++) {
-                        int rgb = image.getRGB(x * widthStep, y * heightStep);
-                        newImage.setRGB(x, y, rgb);
-                    }
-                }
+                BufferedImage newImage = Scalr.resize(image,Scalr.Method.AUTOMATIC, Scalr.Mode.AUTOMATIC, newWidth,newHeight);
 
                 File newFile = new File(dstFolder + "/" + file.getName());
                 ImageIO.write(newImage, "jpg", newFile);
