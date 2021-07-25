a504cf468515d2563dfb0c4a94b35c654d28c9b3
Nata <lushnikova-natalia@rambler.ru>
2021-07-25 13:02:47 +0300

    Homework 17.4 - task 1, v1

--- 17_SpringBootWebDevelopment/ToDoList/src/main/java/main/controller/DefaultController.java
+++ 17_SpringBootWebDevelopment/ToDoList/src/main/java/main/controller/DefaultController.java
@@ -1,19 +1,35 @@
 package main.controller;
 
+import main.model.Task;
+import main.service.TaskServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
-@RestController
+@Controller
 public class DefaultController {
 
-    @GetMapping ("/")
-    public String index(@RequestParam String someText, @RequestParam int value) {
-        StringBuilder resultString = new StringBuilder();
-        // for test: http://localhost:8080/?someText=stuff&value=3
-        for (int i = 0; i < value; i++) {
-            resultString.append(someText);
-        }
-        return resultString.toString();
+    @Autowired
+    TaskServiceImpl taskService;
+
+//    @GetMapping ("/")
+//    public String index(@RequestParam String someText, @RequestParam int value) {
+//        StringBuilder resultString = new StringBuilder();
+//        // for test: http://localhost:8080/?someText=stuff&value=3
+//        for (int i = 0; i < value; i++) {
+//            resultString.append(someText);
+//        }
+//        return resultString.toString();
+//    }
+
+    @RequestMapping ("/")
+    public String index(Model model) {
+        List<Task> list = taskService.list();
+        model.addAttribute("tasks", list);
+        return "index";
     }
 }
