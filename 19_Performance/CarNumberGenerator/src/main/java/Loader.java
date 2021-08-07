cc9ca44d6a6e6f05df6b2e23560b391715b2c7af
Nata <lushnikova-natalia@rambler.ru>
2021-08-07 13:06:00 +0300

    Homework 19.> - task 1, v1.3

--- 19_Performance/CarNumberGenerator/src/main/java/Loader.java
+++ 19_Performance/CarNumberGenerator/src/main/java/Loader.java
@@ -12,16 +12,20 @@ public class Loader {
         char letters[] = {'У', 'К', 'Е', 'Н', 'Х', 'В', 'А', 'Р', 'О', 'С', 'М', 'Т'};
         StringBuilder builder = new StringBuilder();
         for (int regionCode = 1; regionCode < 100; regionCode++) {
+            String regionCodeStr = Integer.toString(regionCode);
+            int regionCodeStrLength = regionCodeStr.length();
             builder = new StringBuilder();
             for (int number = 1; number < 1000; number++) {
+                String numberStr = Integer.toString(number);
+                int numberStringLength = numberStr.length();
                 for (char firstLetter : letters) {
                     for (char secondLetter : letters) {
                         for (char thirdLetter : letters) {
                             builder.append(firstLetter);
-                            builder.append(padNumber(number, 3));
+                            builder.append(padNumber(numberStr, numberStringLength,3));
                             builder.append(secondLetter);
                             builder.append(thirdLetter);
-                            builder.append(padNumber(regionCode, 2));
+                            builder.append(padNumber(regionCodeStr, regionCodeStrLength, 2));
                             builder.append("\n");
                         }
                     }
@@ -38,24 +42,11 @@ public class Loader {
 
 
 
-    private static String padNumber(int number, int numberLength) {
-//        String numberStr = Integer.toString(number);
-//        int padSize = numberLength - numberStr.length();
-//        for (int i = 0; i < padSize; i++) {
-//            numberStr = '0' + numberStr;
-//        }
-//
-//        return numberStr;
-
-        String numberStr = Integer.toString(number);
-        StringBuilder numberStrBuilder = new StringBuilder();
-
-        int padSize = numberLength - numberStr.length();
+    private static String padNumber(String numberStr, int numberStringLength, int numberLength) {
+        int padSize = numberLength - numberStringLength;
         for (int i = 0; i < padSize; i++) {
-            numberStrBuilder.append("0");
+            numberStr = '0' + numberStr;
         }
-        numberStrBuilder.append(numberStr);
-
-        return numberStrBuilder.toString();
+        return numberStr;
     }
 }
